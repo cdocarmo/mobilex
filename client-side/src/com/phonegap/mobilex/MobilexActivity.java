0066e8089d904d13033b5fac3918e1a943cc73d9
Gabriel Fernández <gabofer82@gmail.com>
2012-04-30 10:21:53 -0300

    Se configuró el client-side para que ya se pueda probar sobre Android.

--- client-side/src/com/phonegap/mobilex/MobilexActivity.java
+++ client-side/src/com/phonegap/mobilex/MobilexActivity.java
@@ -1,13 +1,15 @@
 package com.phonegap.mobilex;
 
-import android.app.Activity;
+//import android.app.Activity;
 import android.os.Bundle;
+import org.apache.cordova.*;
 
-public class MobilexActivity extends Activity {
+public class MobilexActivity extends DroidGap {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        //setContentView(R.layout.main);
+        super.loadUrl("file:///android_asset/www/index.html");
     }
 }
